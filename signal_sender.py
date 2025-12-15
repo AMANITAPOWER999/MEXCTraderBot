5b93912ec0f238050bfcd3df418a121cf55d0e88
AMANITAPOWER999 <poweramanita@gmail.com>
2025-12-15 21:05:01 +0700

    Update signal_sender.py

--- signal_sender.py
+++ signal_sender.py
@@ -41,7 +41,7 @@ class SignalSender:
             "settings": {
                 "targetUrl": self.target_url,
                 "openType": position_capitalized,
-                "openPercent": 50,
+                "openPercent": 33,
                 "closeType": position_capitalized,
                 "closePercent": 100,
                 "mode": mode
